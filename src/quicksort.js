6b3734ea2ac288272580c39e7592dfb0fd2b5d0e
Jude Lawson <lawsonjdutton@gmail.com>
2018-08-12 11:42:04 -0600

    Updates WIP quicksort algorithm and tests

--- src/quicksort.js
+++ src/quicksort.js
@@ -1,5 +1,18 @@
-const quicksort = () => {
-  return 'quicksort';
+const quicksort = (arr) => {
+  let pivot = arr[arr.length - 1];
+  let sortedArray = sortIt(arr, pivot);
+  return sortedArray;
+};
+
+const sortIt = (currentArr, pivot) => {
+  // Right side of pivot first
+  let pivotIndex = currentArr.findIndex(pivot)
+  console.log(pivotIndex);
+  let right = currentArr.slice(pivotIndex);
+  console.log(right)
+  // Left side of pivot next
+  // Base Case: If all on right is greater than pivot AND all on left is greater than pivot
+  // Else call function again
 };
 
 module.exports = quicksort;
--- tests/quicksort_test.js
+++ tests/quicksort_test.js
@@ -2,7 +2,7 @@ const assert = require('chai').assert;
 const quicksort = require('../src/quicksort');
 
 describe('Quicksort', () => {
-  it('should return the word quicksort', () => {
-    assert.equal(quicksort(), 'quicksort');
+  it('should be able to sort a simple array of numbers', () => {
+    assert(quicksort([2,1]), [1,2]);
   });
 });
