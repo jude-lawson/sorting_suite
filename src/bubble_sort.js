109312efb9a42e89675b11113e626f9a514be071
Jude Lawson <lawsonjdutton@gmail.com>
2018-08-12 11:42:28 -0600

    Implements initial small example bubble sort algorithm

--- src/bubble_sort.js
+++ src/bubble_sort.js
@@ -1,5 +1,18 @@
-const bubbleSort = () => {
-  return 'Bubble Sort';
+const bubbleSort = (arr) => {
+  let previousIndex = 0
+  let nextIndex = 1
+  let previousNumber = arr[previousIndex];
+  let nextNumber = arr[nextIndex];
+
+  console.log(arr);
+
+  if (previousNumber > nextNumber) {
+    arr[previousIndex] = nextNumber;
+    arr[nextIndex] = previousNumber;
+    bubbleSort(arr);
+  }
+
+  return arr;
 };
 
 module.exports = bubbleSort;
--- tests/bubble_sort_test.js
+++ tests/bubble_sort_test.js
@@ -1,7 +1,9 @@
 const assert = require('chai').assert;
 const bubbleSort = require('../src/bubble_sort');
+
+
 describe('Bubble Sort', () => {
-  it('should return a simple string for now', () => {
-    assert.equal(bubbleSort(), 'Bubble Sort');
-  });
+  it('should be able to sort a simple array in ascending order', () => {
+    assert.deepEqual(bubbleSort([2,1]), [1,2])
+  })
 });
